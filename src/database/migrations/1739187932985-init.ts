632b4b88caaeea56a0e5a4c87a1293cf1d3b750e
AndriySysoenko <gaskonets@gmail.com>
2025-02-10 15:07:23 +0200

    During the final check it was found that deleting a user gave an error related to incorrectly configured connections with the refresh_token table. The problem was fixed.

---
+++ src/database/migrations/1739187932985-init.ts
@@ -0,0 +1,26 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export class Init1739187932985 implements MigrationInterface {
+    name = 'Init1739187932985'
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`ALTER TABLE "user" DROP CONSTRAINT "FK_7c91492fa6749e6d222216fa874"`);
+        await queryRunner.query(`ALTER TABLE "refresh_tokens" DROP CONSTRAINT "FK_610102b60fea1455310ccd299de"`);
+        await queryRunner.query(`ALTER TABLE "user" ALTER COLUMN "name" SET NOT NULL`);
+        await queryRunner.query(`ALTER TABLE "refresh_tokens" ALTER COLUMN "userId" SET NOT NULL`);
+        await queryRunner.query(`ALTER TABLE "refresh_tokens" ALTER COLUMN "token" SET NOT NULL`);
+        await queryRunner.query(`ALTER TABLE "user" ADD CONSTRAINT "FK_7c91492fa6749e6d222216fa874" FOREIGN KEY ("refreshTokenId") REFERENCES "refresh_tokens"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`);
+        await queryRunner.query(`ALTER TABLE "refresh_tokens" ADD CONSTRAINT "FK_610102b60fea1455310ccd299de" FOREIGN KEY ("userId") REFERENCES "user"("id") ON DELETE CASCADE ON UPDATE NO ACTION`);
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`ALTER TABLE "refresh_tokens" DROP CONSTRAINT "FK_610102b60fea1455310ccd299de"`);
+        await queryRunner.query(`ALTER TABLE "user" DROP CONSTRAINT "FK_7c91492fa6749e6d222216fa874"`);
+        await queryRunner.query(`ALTER TABLE "refresh_tokens" ALTER COLUMN "token" DROP NOT NULL`);
+        await queryRunner.query(`ALTER TABLE "refresh_tokens" ALTER COLUMN "userId" DROP NOT NULL`);
+        await queryRunner.query(`ALTER TABLE "user" ALTER COLUMN "name" DROP NOT NULL`);
+        await queryRunner.query(`ALTER TABLE "refresh_tokens" ADD CONSTRAINT "FK_610102b60fea1455310ccd299de" FOREIGN KEY ("userId") REFERENCES "user"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`);
+        await queryRunner.query(`ALTER TABLE "user" ADD CONSTRAINT "FK_7c91492fa6749e6d222216fa874" FOREIGN KEY ("refreshTokenId") REFERENCES "refresh_tokens"("id") ON DELETE SET NULL ON UPDATE NO ACTION`);
+    }
+
+}
